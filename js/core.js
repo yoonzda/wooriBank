e902e6150e1a3521f9a55f43bc5135c6c0806759
yoonzda <geeyounjee@gmail.com>
2025-11-18 04:32:07 +0900

    [WORK-1] - faq com

--- js/core.js
+++ js/core.js
@@ -1,5 +1,12 @@
 $('document').ready(function(){
     selectCustom();
+    $('.accordion .faq_answer_box').hide();
+    $('.accordion h4').click(function() {
+        $('.faq_answer_box').stop().slideUp(300);
+        $(this).parents().siblings().removeClass('active');
+        $(this).next('.faq_answer_box').stop().slideToggle(300);
+        $(this).parents().toggleClass('active');
+    });
 });
 
 function selectCustom(){
